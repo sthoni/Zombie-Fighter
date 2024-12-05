ce59055ce7d5ae207537071265225d1c1ba1013f
Pinkman85 <nicusirbu555@gmail.com>
2024-12-05 09:09:16 +0100

    weißer Rechteck gelöscht

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ class App:
         self.rect_x = 248
         self.rect_y = 248
         
-        self.rect_xx = 100
-        self.rect_yy = 100
-        
         self.backgroundColor = 0
         pyxel.run(self.update, self.draw)
 
@@ -16,9 +13,6 @@ class App:
 
         if self.rect_y < 768 - 248:
             self.rect_y +=10
-
-        if self.rect_yy < 768 - 248:
-            self.rect_yy += 10
 
         if pyxel.btn(pyxel.KEY_D):
             if self.rect_x < pyxel.width - 100:
@@ -31,29 +25,11 @@ class App:
         if pyxel.btn(pyxel.KEY_W):   
             pass
 
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            if self.rect_xx < pyxel.width - 100:
-                self.rect_xx += 10
-            print("Rechts")
-        if pyxel.btn(pyxel.KEY_LEFT):
-            if self.rect_xx > 0:
-                self.rect_xx -= 10
-            print("Links")
-        if pyxel.btn(pyxel.KEY_DOWN):   
-            pass
-        if pyxel.btn(pyxel.KEY_UP):   
-            pass
-
-                
-        
-        
-
     def draw(self):
         pyxel.cls(0)
         
         
         pyxel.rect(self.rect_x, self.rect_y, 100, 248, 2)
         
-        pyxel.rect(self.rect_xx, self.rect_yy, 100, 248, 7)
 
 App()
