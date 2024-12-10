48f820811d710592b394e4364d07e4d90bb1d799
Birkenapps <81261812+MateOreo@users.noreply.github.com>
2024-12-10 09:18:48 +0100

    Gravitation + Jump
    Mit Beschleunigung Gravitation

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ class App:
     def __init__(self):
         pyxel.init(1024, 768)
         self.rect_x = 248
-        self.rect_y = 248
-        
+        self.rect_y = 50
+
+        self.velocity = 0
+        self.gravity = 1
+
         self.backgroundColor = 0
         pyxel.run(self.update, self.draw)
 
-        return self.rect_x
 
     def update(self):
+        
+        #Gravitation
+        self.velocity += self.gravity
+        self.rect_y += self.velocity
 
-        if self.rect_y < 768 - 248:
-            self.rect_y +=10
-
+        if self.rect_y + 248 >= pyxel.height:  #248 is how big the player on the y axe is
+            self.rect_y = pyxel.height - 248
+            self.velocity = 0
+        
+        #Movement
         self.rect_x = movement.movementP1(self.rect_x)
 
-        
-        
-
-    	
+        if self.rect_y == 768 - 248:
+            self.rect_y = movement.jump(self.rect_y)
+ 
 
     def draw(self):
         pyxel.cls(0)
--- movement.py
+++ movement.py
@@ -3,6 +3,17 @@ import pyxel
 
 def movementP1(xPos):
     if pyxel.btn(pyxel.KEY_D):
-        print(xPos)
         xPos += 20
+
+    elif pyxel.btn(pyxel.KEY_A):
+        xPos -= 20
+
     return xPos
+
+def jump(yPos):
+    jumpVelocity = 0
+
+
+    if pyxel.btn(pyxel.KEY_W):
+        yPos -= jumpVelocity
+    return yPos
