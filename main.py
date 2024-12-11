f17dd44e73bc89c410814bfc8214da157d9b4f99
Birkenapps <81261812+MateOreo@users.noreply.github.com>
2024-12-11 21:02:11 +0100

    Schlag mit Cooldown
    Wir müssen noch Herr thpn fragen ob die lösung gut ist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyxel
 import movement
+import time
 
 class App:
     def __init__(self):
@@ -10,6 +11,10 @@ class App:
         self.velocity = 0
         self.gravity = 1
 
+        self.p1IsPunching = False
+        self.p1LastPunchTime = 0 # Zeit vom letzten Schlag
+        self.p1CurrentPunchTime = 0 # Zeit vom jetzt versuchten Schlag
+
         self.backgroundColor = 0
         pyxel.run(self.update, self.draw)
 
@@ -17,18 +22,36 @@ class App:
     def update(self):
         
         #Gravitation
+
         self.velocity += self.gravity
         self.rect_y += self.velocity
 
-        if self.rect_y + 248 >= pyxel.height:  #248 is how big the player on the y axe is
+        if self.rect_y + 248 >= pyxel.height:  #248 ist die Größe vom Spieler auf der Y-Achse
             self.rect_y = pyxel.height - 248
             self.velocity = 0
         
+
+
         #Movement
+
         self.rect_x = movement.movementP1(self.rect_x)
 
         if self.rect_y == 768 - 248:
             self.rect_y = movement.jump(self.rect_y)
+
+
+
+        # Schlag
+
+        if pyxel.btnp(pyxel.KEY_SPACE) and int(time.time()) - self.p1LastPunchTime >= 1:
+            print("Differenz: " , int(time.time()) - self.p1LastPunchTime)
+            self.p1IsPunching = True
+            self.p1LastPunchTime = int(time.time())
+        else: # Wir müssen Herr Thon fragen, ob das eine gute Lösung ist
+            self.p1IsPunching = False
+
+
+            
  
 
     def draw(self):
@@ -36,6 +59,9 @@ class App:
         
         
         pyxel.rect(self.rect_x, self.rect_y, 100, 248, 2)
+
+        if self.p1IsPunching:
+            pyxel.rect(self.rect_x + 160, self.rect_y + 5, 50,50, 8)
         
 
 App()
