6ba11f2c58871cf936d0c9125ad0f0801ad2661a
Pinkman85 <nicusirbu555@gmail.com>
2024-12-12 09:04:36 +0100

    player punch function
    2 player punch made

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ class App:
         self.rect_x1 = 250
         self.rect_y1 = 50
 
+        self.velocity_p1 = 0
+
+        self.p1IsPunching = False
+        self.p1LastPunchTime = time.time() - 3 # Zeit vom letzten Schlag
+
         # Player 2
         self.rect_x2 = 750
         self.rect_y2 = 50
 
-        self.velocity_p1 = 0
         self.velocity_p2 = 0
         
         self.gravity = 1
 
-        self.p1IsPunching = False
-        self.p1LastPunchTime = time.time() - 3 # Zeit vom letzten Schlag
-        self.p1CurrentPunchTime = 0 # Zeit vom jetzt versuchten Schlag
+        self.p2IsPunching = False
+        self.p2LastPunchTime = time.time() - 3 # Zeit vom letzten Schlag
 
         self.backgroundColor = 0
         pyxel.run(self.update, self.draw)
@@ -45,15 +48,10 @@ class App:
 
         # Schlag
 
-        if pyxel.btnp(pyxel.KEY_SPACE) and time.time() - self.p1LastPunchTime >= 0.5:
-            print("Differenz: " , time.time() - self.p1LastPunchTime)
-            self.p1IsPunching = True
-            self.p1LastPunchTime = time.time()
-        else: # Wir müssen Herr Thon fragen, ob das eine gute Lösung ist
-            self.p1IsPunching = False
-       
- 
+        self.p1IsPunching, self.p1LastPunchTime =  movement.punch_p1(self.p1IsPunching, self.p1LastPunchTime)
 
+        self.p2IsPunching, self.p2LastPunchTime =  movement.punch_p2(self.p2IsPunching, self.p2LastPunchTime)
+        
     def draw(self):
         pyxel.cls(0)
         
@@ -63,7 +61,9 @@ class App:
         pyxel.rect(self.rect_x2, self.rect_y2, 100, 248, 2)
 
         if self.p1IsPunching:
-            pyxel.rect(self.rect_x1 + 160, self.rect_y1 + 5, 50,50, 8)
+            pyxel.rect(self.rect_x1 + 160, self.rect_y1 + 5, 50, 50, 8)
+        if self.p2IsPunching:
+            pyxel.rect(self.rect_x2 + 160, self.rect_y2 + 5, 50, 50, 8)
         
 
 App()
--- movement.py
+++ movement.py
@@ -1,4 +1,5 @@
 import pyxel
+import time
 
 
 def movementP1(xPos):
@@ -28,11 +29,29 @@ def jump(yPos):
     return yPos
 
 def gravitation(rect_y, height, gravity, velocity):
-        if rect_y + 248 <= height:
-            velocity += gravity
-            rect_y += velocity
+    if rect_y + 248 <= height:
+        velocity += gravity
+        rect_y += velocity
 
-        if rect_y + 248 >= height:  #248 ist die Größe vom Spieler auf der Y-Achse
-            velocity = 0
+    if rect_y + 248 >= height:  #248 ist die Größe vom Spieler auf der Y-Achse
+        velocity = 0
 
-        return rect_y, velocity
+    return rect_y, velocity
+
+def punch_p1(pIsPunching, pLastPunchTime):
+    if pyxel.btnp(pyxel.KEY_G) and time.time() - pLastPunchTime >= 0.5:
+        print("Differenz: " , time.time() - pLastPunchTime)
+        pIsPunching = True
+        pLastPunchTime = time.time()
+    else: # Wir müssen Herr Thon fragen, ob das eine gute Lösung ist
+        pIsPunching = False
+    return pIsPunching, pLastPunchTime
+
+def punch_p2(pIsPunching, pLastPunchTime):
+    if pyxel.btnp(pyxel.KEY_KP_5) and time.time() - pLastPunchTime >= 0.5:
+        print("Differenz: " , time.time() - pLastPunchTime)
+        pIsPunching = True
+        pLastPunchTime = time.time()
+    else: # Wir müssen Herr Thon fragen, ob das eine gute Lösung ist
+        pIsPunching = False
+    return pIsPunching, pLastPunchTime
