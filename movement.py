25f7acb507e2774f3e9f7eaf1e02fe87d157a99b
Pinkman85 <nicusirbu555@gmail.com>
2025-01-05 17:11:22 +0100

    block fix

--- movement.py
+++ movement.py
@@ -62,21 +62,22 @@ def gravitation(rect_y, velocity, gravity):
 def punch_p1(self, p_is_punching, p_last_punch_time, enemy_x, player_x, hp2): #pIsPunching: p1_is_punching (Für Hitbox)   p_last_punch_time: Cooldoown Zeit (Letzter Schlag)
     if pyxel.btnp(pyxel.KEY_SPACE) and time.time() - p_last_punch_time >= 0.5: # Geguckt ob G gedrückt wird und Cooldown
         p_is_punching = True #Aktiviert die Hitbox
-        pyxel.play(3,51)
         p_last_punch_time = time.time() # Letzter Schlag (Zeit) wird auf jetzt gesetzt
 
         if collision_rechts(player_x, enemy_x):
             if self.state_player_2 == "normal" and self.state_player_1 == "normal":
                 hp2 -= 80
-            else:
+                pyxel.play(3,51)
+            elif self.state_player_2 == "blocking" and self.state_player_1 == "normal":
                 hp2 -= 10
+                pyxel.play(3,51)
             print("P2: " , hp2)
             print("Rechts")
 
         if collision_links(player_x, enemy_x):
             if self.state_player_2 == "normal" and self.state_player_1 == "normal":
                 hp2 -= 80
-            else:
+            elif self.state_player_2 == "blocking" and self.state_player_1 == "normal":
                 hp2 -= 10
             print("P2: " , hp2)
             print("Links")
@@ -99,7 +100,7 @@ def punch_p2(self, p_is_punching, p_last_punch_time, enemy_x, player_x, hp1):
         if collision_rechts(player_x, enemy_x):
             if self.state_player_1 == "normal" and self.state_player_2 == "normal":
                 hp1 -= 80
-            else:
+            elif self.state_player_1 == "blocking" and self.state_player_2 == "normal":
                 hp1 -= 10
             print("P1: ", hp1)
             print("Rechts")
@@ -107,7 +108,7 @@ def punch_p2(self, p_is_punching, p_last_punch_time, enemy_x, player_x, hp1):
         if collision_links(player_x, enemy_x):
             if self.state_player_1 == "normal" and self.state_player_2 == "normal":
                 hp1 -= 80
-            else:
+            elif self.state_player_1 == "blocking" and self.state_player_2 == "normal":
                 hp1 -= 10
             print("P1: " , hp1)
             print("Links")
